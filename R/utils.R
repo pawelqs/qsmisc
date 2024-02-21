024d7211ace45881faaf02172ff7a2aa369af2bb
Paweł <kpawel2210@gmail.com>
2024-02-21 20:06:19 +0100

    quick_save and quick_load added

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,24 @@
 #' @param y right-hand side argument
 #' @export
 '%not in%' <- function(x,y)!('%in%'(x,y))
+
+
+#' Quick save of a single object
+#'
+#' Saves object to ~/.qstools directory, together with quick_load() allows
+#' to quickly move object between R sessions (e.g. between the analysis and
+#' package debuggin session)
+#'
+#' @param object object to save
+#' @export
+quick_save <- function(object) {
+  dir.create("~/.qstools", showWarnings = FALSE)
+  saveRDS(object, "~/.qstools/object.Rds")
+}
+
+
+#' @describeIn quick_save Quick load of ~/.qstools/object.Rds
+#' @export
+quick_load <- function() {
+  readRDS("~/.qstools/object.Rds")
+}
